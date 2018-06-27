415b7daab947fbf37db128e756751c337a91d0ad
Kartik Iyyer <kiyyer@juniper.net>
2018-06-27 16:25:49 -0700

    Taking port from environment variable

--- hello_world.py
+++ hello_world.py
@@ -12,4 +12,4 @@ def hello_world():
 	return 'Hello World!'
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=int(os.environment.get('PORT', 5000)))
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
